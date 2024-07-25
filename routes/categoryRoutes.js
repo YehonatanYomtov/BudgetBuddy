897b6d261246fc8b8c67bc3370afb53c15256403
Meirdavv <meirbusinessac@gmail.com>
2024-07-25 10:55:26 +0300

    added models controllers and routes for category, finished working version with postman and ejs

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const categoryController = require('../controllers/categoryController.js')
 
-router.get("/", (req, res) => {
-  res.render("manageCategories", {
-    title: "Manage Categories",
-    text: "Yay we did it!",
-  });
-});
+// router.get("/", (req, res) => {
+//   res.render("manageCategories", {
+//     title: "Manage Categories",
+//     text: "Yay we did it!",
+//   });
+// });
+
+router.get('/user/:user_id', categoryController.getCategoriesByUserId);
+router.post('/', categoryController.createCategory);
+router.post('/delete/:id', categoryController.deleteCategory);
 
 module.exports = router;
