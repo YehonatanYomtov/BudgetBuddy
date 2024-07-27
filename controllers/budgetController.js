11381ba35fb30b4ac75a241d582bc0ff19d99f79
Meirdavv <meirbusinessac@gmail.com>
2024-07-27 22:21:06 +0300

    continueing previous commit

---
+++ controllers/budgetController.js
@@ -0,0 +1,26 @@
+const {Budget} = require("../models/budgetModel.js");
+
+const getBudget = async (req,res) => {
+    const { user_id } = req.params;
+
+    try{
+        const expenses = await Budget.getTotalExpenses(user_id) || 0 ;
+        const incomes = await Budget.getTotalIncomes(user_id) || 0;
+        const balance = (incomes - expenses).toFixed(2);
+        res.render('budgetSummary', {
+            title: 'Budget Summary',
+            incomes,
+            expenses,
+            balance,
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error: "Error fetching budget summary."});
+    }
+}
+
+
+module.exports = {
+    getBudget,
+}
+
---
+++ routes/budgetRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const budgetController = require('../controllers/budgetController.js');
+
+router.get("/user/:user_id", budgetController.getBudget);
+
+module.exports = router;
